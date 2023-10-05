9389d4a62ea51493a4686157a85d16c7964d0ba5
Elise <elise.chaldjian@gmail.com>
2023-10-05 23:36:05 +0200

    Quête 2 - Sécurisation

--- form.php
+++ form.php
@@ -3,30 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Form</title>
 </head>
 <body>
+
     <form action="thanks.php" method="POST">
     <div>
     <label for ="lname">Nom :</label>  
-    <input type ="text" name="lname" id="lname">
+    <input type ="text" name="lname" id="lname" required=true>
         </div>    
     <div>
     <label for ="fname">Prénom :</label>
-    <input type ="text" name="fname" id="fname">
+    <input type ="text" name="fname" id="fname" required=true>
         </div>
         <div>
     <label for ="mail">E-mail :</label>
-    <input type ="email" name="mail" id="mail">
+    <input type ="email" name="mail" id="mail" required=true>
         </div>
         <div>
     <label for ="pNumber">Numéro de téléphone :</label>   
-    <input type ="text" name="pNumber" id="pNumber">
+    <input type ="text" name="pNumber" id="pNumber" required=true>
         </div>
 
         <div>
         <label for="sujet">Sujet :</label>
-    <select name="userSelect" id="sujet">
+    <select name="userSelect" id="sujet" required=true>
     <option value="">Choisissez une raison</option>;
     <option value="Perte">Perte de votre commande</option>;
     <option value="Livraison">Durée de livraison</option>;
@@ -34,7 +35,7 @@
 </div>
         <div>
     <label for ="userMessage">Votre message :</label>
-    <textarea name="userMessage" id="userMessage"></textarea>
+    <textarea name="userMessage" id="userMessage" required=true></textarea>
         </div>
 
         <div class="button">
--- thanks.php
+++ thanks.php
@@ -1,7 +1,54 @@
-<?php 
-echo "Merci " . $_POST['fname'] . " " . $_POST['lname'] . " de nous avoir contacté à propos de " . $_POST['userSelect'] .
-"<br>" .
-"Un de nos conseillers vous contactera soit à l’adresse " . $_POST['mail'] . " ou par téléphone au " . $_POST['pNumber'] . " dans les plus brefs délais pour traiter votre demande :<br>" . 
+<?php $errors= [];
 
-$_POST['userMessage']
+$data = array_map('trim', $_POST);
+$data = array_map('htmlentities', $data);
+
+    if(!isset($data['lname']) || empty($data['lname'])){
+        $errors[]= 'Le champ est obligatoire';
+    };
+
+    if(!isset($data['fname']) || empty($data['fname'])){
+        $errors[]='Le champ est toujours obligatoire';
+    };
+
+    if(!filter_var($data['mail'], FILTER_VALIDATE_EMAIL)){
+        $errors[]='Poupilou poupou';
+    };
+
+    if(!isset($data['pNumber']) || empty($data['pNumber'])){
+        $errors[]='Do you know what a phone number is ?';
+    };
+
+    if(!isset($data['userSelect']) || empty($data['userSelect'])){
+        $errors[]='On est pas devin, on est censé t\'aider comment ?';
+    };
+
+    if(!isset($data['userMessage']) || empty($data['userMessage'])){
+        $errors[]='Bon, that is enough.';
+    };
+
+    if (!empty($errors)) {
+    require 'error.php';
+    die();
+    }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+echo "Merci " . $data['fname'] . ' ' . $data['lname'] . " de nous avoir contacté à propos de " . $data['userSelect'] . '.'.
+
+" Un de nos conseillers vous contactera soit à l’adresse " . $data['mail'] . " ou par téléphone au " . $data['pNumber'] . " dans les plus brefs délais pour traiter votre demande : " . $data['userMessage'];
+
+?>    
+</body>
+</html>
+
+</body>
+</html>
